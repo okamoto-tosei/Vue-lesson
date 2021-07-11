aa2606a7bca79df9e9eb0b01692fa1ef79f819be
okamoto-tosei <tosei393939@gmail.com>
2021-07-12 02:30:30 +0900

    for in item [reactでいうmapを使用しての表示]をVueで再現

--- app.js
+++ app.js
@@ -5,14 +5,29 @@ const app = Vue.createApp({
       title: 'The title',
       author: 'Brandon Sanderson',
       age: 45,
+      x: 0,
+      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss' },
+        { title: 'the way of kings', author: 'brandon sanderson' },
+        { title: 'the final empire', author: 'brandon sanderson' },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
-    handleEvent(e) {
+    handleEvent(e, data) {
       console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      console.log(e);
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
